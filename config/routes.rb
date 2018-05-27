76b4f9a476a1e81faadba6ab4b3be08c4f03296c
Yony Tan <myonys@gmail.com>
2018-05-27 14:25:45 +0000

    Changes to the Readme file.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
 root "pages#home"
-get "pages/about", to: "pages#about"
+get "about", to: "pages#about"
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
